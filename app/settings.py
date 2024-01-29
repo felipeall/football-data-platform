c4dd00755d25509a1f90b12af8c91c7af063feb3
Felipe Allegretti <felipeallh@gmail.com>
2024-01-28 22:41:47 -0300

    Add Postgres and MinIO env vars

--- app/settings.py
+++ app/settings.py
@@ -6,6 +6,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
+    # Postgres
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_DB: str = "postgres"
+
+    # MinIO
+    MINIO_ROOT_USER: Optional[str] = "admin"
+    MINIO_ROOT_PASSWORD: Optional[str] = "Admin@123"
+    MINIO_DEFAULT_BUCKET: Optional[str] = "football-data-platform"
+
+    # AWS
     AWS_BUCKET_NAME: str = "football-data-platform"
     AWS_ACCESS_KEY_ID: str = "admin"
     AWS_SECRET_ACCESS_KEY: str = "Admin@123"
