5416fa7e24c299e51f335ed52a7f384e3d84d3dd
Felipe Allegretti <felipeallh@gmail.com>
2024-01-29 01:17:12 -0300

    Implement load_dict Database method

--- app/services/db.py
+++ app/services/db.py
@@ -4,7 +4,7 @@ from typing import Optional
 import pandas as pd
 from pandas.io.sql import SQLTable
 from sqlalchemy import Connection, Engine, MetaData, Table, create_engine
-from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.dialects.postgresql import Insert, insert
 from sqlalchemy.orm import sessionmaker
 
 from app.models.base import Base
@@ -20,24 +20,25 @@ class Database:
         self.engine = create_engine(settings.SQLALCHEMY_DATABASE_URI)
         self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
 
-    @staticmethod
-    def create_upsert_method(meta: MetaData):
+    def create_upsert_method(self, meta: MetaData):
         def method(table: SQLTable, conn: Connection, keys: list[str], data_iter: zip):
             sql_table = Table(table.name, meta, schema=table.schema)
             values_to_insert = [dict(zip(keys, data)) for data in data_iter]
-            insert_stmt = insert(sql_table).values(values_to_insert)
-            update_stmt = {
-                c.name: c
-                for c in insert_stmt.excluded
-                if not c.primary_key and c.name not in ["created_at", "updated_at"]
-            }
-            upsert_stmt = insert_stmt.on_conflict_do_update(
-                index_elements=sql_table.primary_key.columns,
-                set_=update_stmt,
-            )
+            upsert_stmt = self.build_upsert_stmt(sql_table, values_to_insert)
             conn.execute(upsert_stmt)
 
         return method
+
+    @staticmethod
+    def build_upsert_stmt(sql_table: Table, values_to_insert) -> Insert:
+        insert_stmt = insert(sql_table).values(values_to_insert)
+        update_stmt = {
+            c.name: c for c in insert_stmt.excluded if not c.primary_key and c.name not in ["created_at", "updated_at"]
+        }
+        return insert_stmt.on_conflict_do_update(
+            index_elements=sql_table.primary_key.columns,
+            set_=update_stmt,
+        )
 
     @property
     def upsert(self):
@@ -52,3 +53,9 @@ class Database:
             schema=schema,
             method=self.upsert,
         )
+
+    def load_dict(self, data: dict, table: str, schema: Optional[str] = None) -> None:
+        sql_table = Table(table, Base.metadata, schema=schema)
+        upsert_stmt = self.build_upsert_stmt(sql_table, [data])
+        with self.engine.begin() as conn:
+            conn.execute(upsert_stmt)
